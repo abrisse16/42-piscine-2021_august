968f1ef3355b7aa0bba35bb9dd201dc45257b1bf
Alexis Brisse <alexisbrisse@gmail.com>
2021-08-04 12:19:00 +0200

    Shell 01 : first try version

---
+++ Shell01/ex07/r_dwssap.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+cat /etc/passwd | grep -ve '#' | awk '{if (NR%2 ==1) print $0}' | cut -d: -f 1 | rev | sort -r | awk '{if (NR>='$FT_LINE1' && NR<='$FT_LINE2') print $0}' | tr '\n' ',' | sed 's/,/, /g' | sed 's/, $/\./'

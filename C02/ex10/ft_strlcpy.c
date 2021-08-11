9bb96e20654aafc5e5db2b13051507273207ce4d
Alexis Brisse <alexisbrisse@gmail.com>
2021-08-11 12:11:00 +0200

    C 02 : retry 2 version

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: abrisse <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 12:38:30 by abrisse           #+#    #+#             */
-/*   Updated: 2021/08/10 14:17:26 by abrisse          ###   ########.fr       */
+/*   Updated: 2021/08/11 12:10:04 by abrisse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,26 +28,14 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	unsigned int	i;
 
 	i = 0;
-	if (size == 0)
+	if (size > 0)
 	{
-		dest[i] = 0;
-		return (ft_strlen(src));
+		while (i < (size - 1) && *(src + i) != '\0')
+		{
+			*(dest + i) = *(src + i);
+			i++;
+		}
+		*(dest + i) = '\0';
 	}
-	while (i < (size - 1) && *(src + i) != '\0')
-	{
-		*(dest + i) = *(src + i);
-		i++;
-	}
-	*(dest +  i) = '\0';
-	return (ft_strlen(src));
-}
-
-int main(int ac, char **qv)
-{
-	char	ui[64];
-	char	ui2[64];
-
-	printf("me ret => %d %s\n", ft_strlcpy(ui, qv[1], atoi(qv[2])), ui);
-	printf("you ret => %lu %s\n", strlcpy(ui2, qv[1], atoi(qv[2])), ui2);
-	return (0);
+	return ((unsigned int)ft_strlen(src));
 }

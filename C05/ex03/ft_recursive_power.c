4a2ab2ad63fa10e0dd38cfde9806e1764fb05049
Alexis Brisse <alexisbrisse@gmail.com>
2021-08-15 12:07:00 +0200

    C 05 : first try version

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abrisse <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/13 13:09:22 by abrisse           #+#    #+#             */
+/*   Updated: 2021/08/15 11:55:42 by abrisse          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	else if (power == 1)
+		return (nb);
+	else
+		return (nb * ft_recursive_power(nb, power - 1));
+}
---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abrisse <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/15 10:42:29 by abrisse           #+#    #+#             */
+/*   Updated: 2021/08/15 12:00:20 by abrisse          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	i = 1;
+	if (nb <= 1)
+		return (0);
+	if (nb == 2 || nb == 3 || nb == 5)
+		return (1);
+	if (nb % 2 == 0 || nb % 5 == 0)
+		return (0);
+	while (i <= nb / i)
+	{
+		i += 2;
+		if (nb % i == 0)
+			return (0);
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	if (nb <= 2)
+		return (2);
+	while (!ft_is_prime(nb))
+		nb++;
+	return (nb);
+}

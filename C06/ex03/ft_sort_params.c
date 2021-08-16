02e012cd4ed371d53993bc82c2060032c59dbc88
Alexis Brisse <alexisbrisse@gmail.com>
2021-08-16 16:15:00 +0200

    C 06 : retry 1 version

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abrisse <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/16 10:44:58 by abrisse           #+#    #+#             */
+/*   Updated: 2021/08/16 15:16:22 by abrisse          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = -1;
+	while (str[++i])
+		write(1, &str[i], 1);
+}
+
+void	ft_strswap(char **str1, char **str2)
+{
+	char	*temp;
+
+	temp = *str1;
+	*str1 = *str2;
+	*str2 = temp;
+}
+
+int	ft_strcmp(char *str1, char *str2)
+{
+	int	i;
+
+	i = 0;
+	while (str1[i] && str1[i] == str2[i])
+		i++;
+	return (str1[i] - str2[i]);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+				ft_strswap(&argv[i], &argv[j]);
+			j++;
+		}
+		i++;
+	}
+	i = 0;
+	while (++i < argc)
+	{
+		ft_putstr(argv[i]);
+		write(1, "\n", 1);
+	}
+}

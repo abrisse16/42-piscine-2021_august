d5548d9154b91d6c896dbbab23716b9eca4778f3
Alexis Brisse <alexisbrisse@gmail.com>
2021-08-24 12:56:00 +0200

    C 09 : first try version

---
+++ C09/ex02/ft_split.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abrisse <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/24 11:54:30 by abrisse           #+#    #+#             */
+/*   Updated: 2021/08/24 12:40:25 by abrisse          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	char_is_charset(char c, char *charset)
+{
+	int	i;
+
+	i = -1;
+	while (charset[++i])
+		if (charset[i] == c)
+			return (1);
+	return (0);
+}
+
+int	get_word_count(char *str, char *charset)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		while (char_is_charset(str[i], charset))
+			i++;
+		if (!char_is_charset(str[i], charset) && str[i])
+		{
+			count++;
+			while (!char_is_charset(str[i], charset) && str[i])
+				i++;
+		}
+	}
+	return (count);
+}
+
+char	*get_word(int *index, char *str, char *charset)
+{
+	int		lenght;
+	char	*word;
+	int		i;
+
+	i = 0;
+	lenght = 0;
+	while (char_is_charset(str[*index], charset))
+		(*index)++;
+	while (!char_is_charset(str[*index + lenght], charset)
+		&& str[*index + lenght])
+		lenght++;
+	word = malloc((lenght + 1) * sizeof(char));
+	if (!word)
+		return (NULL);
+	while (i < lenght)
+	{
+		word[i++] = str[*index];
+		*index = *index + 1;
+	}
+	word[i] = 0;
+	return (word);
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	char	**result;
+	int		word_count;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	word_count = get_word_count(str, charset);
+	result = malloc((word_count + 1) * sizeof(char *));
+	if (!result)
+		return (NULL);
+	while (i < word_count)
+		result[i++] = get_word(&j, str, charset);
+	result[i] = 0;
+	return (result);
+}
+/*
+int	main(int ac, char **av)
+{
+	char	**tab;
+
+	tab = ft_split(av[1], av[2]);
+	for (;*tab;tab++)
+		printf("%s\n", *tab);
+	return 0;
+}
+*/
